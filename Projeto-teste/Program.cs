4abee92396f1bd09933f29ef3c12e85b1d83b4e6
Isaac Marquetti <isaac.marquetti@gmail.com>
2021-09-20 00:33:25 -0300

    Desejando um bom dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -8,9 +8,9 @@ namespace Projeto_teste
         {
             Console.WriteLine("Olá Mundo!");
 
-            Console.WriteLine("Bom dia!");
-
             Console.WriteLine("Como você está hoje?");
+
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
